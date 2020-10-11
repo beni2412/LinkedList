bba2f45288c3eaee42b1b4875ae82eddb8bf6f46
beni2412 <hardaman97@gmail.com>
2020-10-12 01:49:02 +0530

    [Hardaman] Added: Ability to add nodes at front using addFront method

--- src/main/java/datastructures1/MyLinkedList.java
+++ src/main/java/datastructures1/MyLinkedList.java
@@ -21,4 +21,24 @@ public class MyLinkedList {
 
 	}
 
+	public void addFront(INode node) {
+		if (head == null) {
+			this.head = node;
+			this.tail = node;
+		} else {
+			this.tail.setNext(node);
+			this.tail = tail.getNext();
+		}
+
+	}
+
+	public void print() {
+		INode temp = head;
+		while (temp != null) {
+			System.out.print(temp.getKey() + "->");
+			temp = temp.getNext();
+		}
+		System.out.println("");
+	}
+
 }
--- src/test/java/datastructures1/LinkedListTest.java
+++ src/test/java/datastructures1/LinkedListTest.java
@@ -3,9 +3,10 @@ package datastructures1;
 import org.junit.Test;
 
 import org.junit.Assert;
+
 public class LinkedListTest {
 	@Test
-	public void given3NumberWhenAddedToLinkedListShouldBeAdded() {
+	public void given3NumberWhenAddedToLinkedListShouldBeAddedFromEnd() {
 		Node<Integer> firstNode = new Node<Integer>(70);
 		Node<Integer> secondNode = new Node<Integer>(30);
 		Node<Integer> thirdNode = new Node<Integer>(56);
@@ -13,5 +14,18 @@ public class LinkedListTest {
 		list.addEnd(firstNode);
 		list.addEnd(secondNode);
 		list.addEnd(thirdNode);
+		list.print();
+	}
+
+	@Test
+	public void given3NumberWhenAddedToLinkedListShouldBeAddedFromFront() {
+		Node<Integer> firstNode = new Node<Integer>(56);
+		Node<Integer> secondNode = new Node<Integer>(30);
+		Node<Integer> thirdNode = new Node<Integer>(70);
+		MyLinkedList list = new MyLinkedList();
+		list.addFront(firstNode);
+		list.addFront(secondNode);
+		list.addFront(thirdNode);
+		list.print();
 	}
 }
